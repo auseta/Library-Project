47397984fdeace2d139d1010c0f51b735c6bd72d
Augusto Seta <alseta97@gmail.com>
2022-10-10 19:56:51 -0300

    variables and constructor function created

---
+++ index.js
@@ -0,0 +1,17 @@
+let myLibrary = [];
+
+function Book (title, author, pages) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+}
+
+function addBookToLibrary() {
+
+}
+
+const inputBookTitle = document.querySelector("#title");
+const inputBookAuth = document.querySelector("#auth");
+const inputBookPages = document.querySelector("#pages");
+const inputBookRead = document.querySelector("#read");
+
