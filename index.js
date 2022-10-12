004b54435bd75f257fe23559793ae5d57570ef76
Augusto Seta <alseta97@gmail.com>
2022-10-12 01:49:19 -0300

    functionalities have been added: mark as read / unread, add book and delete book from the list

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 let myLibrary = [];
 let index = 0
 
-
-// Funcion constructora
-function Book (title, auth, pages) {
-  this.title = title;
-  this.auth = auth;
-  this.pages = pages;
-  this.id = 0;
-}
-
-function addBookToLibrary(book) {
-  myLibrary.push(book)
-}
+/* state functions */
 
 function isEmpty() {
   if (inputTitle.value !== "" && inputAuth.value !== "" && inputPages.value !== "") {
@@ -22,41 +11,90 @@ function isEmpty() {
   }
 }
 
+function resetInputs() {
+  inputTitle.value = "";
+  inputAuth.value = "";
+  inputPages.value = "";
+  inputRead.checked = false;
+}
+
+function changeReadStatus(card, state) {
+  let cardIndex = card.getAttribute("data-index")
+  for (let book of myLibrary) {
+    if (book.id === +cardIndex) {
+      book.read = state
+    }
+  }
+}
+
+function changeStatus(e) {
+  let checkCardInput = e.currentTarget
+  let cardBookLabel = e.currentTarget.nextElementSibling
+  let card = e.currentTarget.parentNode.parentNode
+  console.log(card);
+  if (checkCardInput.checked === true) {
+    cardBookLabel.textContent = "Readed"
+    checkCardInput.setAttribute("value", "readed")
+    changeReadStatus(card, true)
+  } else {
+    cardBookLabel.textContent = "Not Readed"
+    checkCardInput.setAttribute("value", "not-readed")
+    checkCardInput.setAttribute("value", "not-readed")
+    changeReadStatus(card, false)
+  }
+}
+
+// constructor function
+function Book (title, auth, pages) {
+  this.title = title;
+  this.auth = auth;
+  this.pages = pages;
+  this.id = 0;
+  this.read = false
+}
+
+// main functions
+function addBookToLibrary(book) {
+  myLibrary.push(book)
+}
+
 function createCardBook(book) {
   let cardContainer = document.createElement("div")
   cardContainer.classList.add("card")
   cardContainer.setAttribute("data-index", book.id)
   cardContainer.innerHTML = `
-    <div class="delete-card">
-      <button onclick="deleteCard(event)" ><img src="./img/close.svg" alt="close"></button>
-    </div>
+    <button onclick="deleteCard(event)">X</button>
     <div class="card-info">
-      <span>${book.title}</span>
+      <h3>${book.title}</h3>
       <span>${book.auth}</span>
-      <span>${book.pages}</span>
+      <span>${book.pages} pages</span>
     </div>
     <div class="card-status">
-      <span></span>
+      <input type="checkbox" ${book.read === true ? "checked" : ""} name="card-status" id="card-status" value="${book.read === true ? "readed" : "not-readed"}" onclick="changeStatus(event)">
+      <label for="card-status">${book.read === true ? "Readed" : "Not Readed"}</label>
     </div>
   `
   mainContainer.appendChild(cardContainer)
 }
 
-// Funciones Principales
 function createCard() {
   let empty = isEmpty()
   if (!empty) {
     let newBook = new Book(inputTitle.value, inputAuth.value, inputPages.value)
+    if (inputRead.checked === true) {
+      newBook.read = true
+    }
     addBookToLibrary(newBook)
     newBook.id = index
     index++
     createCardBook(newBook)
     formContainer.style.display = "none"
+    resetInputs()
   }
 }
 
 function deleteCard(e) {
-  let card = e.currentTarget.parentNode.parentNode
+  let card = e.currentTarget.parentNode
   let cardIndex = card.getAttribute("data-index")
   let libraryUpdate = myLibrary.filter( book => book.id !== +cardIndex )
   myLibrary = libraryUpdate;
